c8cddc5ea7472df09a973913f2ac902b6dde8261
Rhys Morgan <rhysm@me.com>
2024-08-29 19:44:04 +0100

    Initial Commit

---
+++ DatabaseTestApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DatabaseTestApp
+//
+//  Created by Rhys Morgan on 29/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DatabaseTestApp/DatabaseTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatabaseTestAppApp.swift
+//  DatabaseTestApp
+//
+//  Created by Rhys Morgan on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct DatabaseTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
